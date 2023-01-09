91aa454d80856b1418089dc3ffc098a7b15d2a41
Brayden Killeen <119263058+braydenkilleen@users.noreply.github.com>
2023-01-09 17:53:17 +1100

    Alternate fib computation

--- fibonacci.py
+++ fibonacci.py
@@ -7,8 +7,20 @@ def fibonacci(n):
     return (a+b, a)
 
 
+def inefficient_fibonacci(n):
+    """Return the nth fibonacci number. This is inefficient due to repeated work in return statement. TODO: More explanation."""
+    if n <= 1:
+        return n
+    return inefficient_fibonacci(n-2) + inefficient_fibonacci(n-1)
+
+
 if __name__ == '__main__':
-    print(fibonacci(1))
-    print(fibonacci(2))
-    print(fibonacci(3))
-    print(fibonacci(4))
+    # print(fibonacci(1))
+    # print(fibonacci(2))
+    # print(fibonacci(3))
+    # print(fibonacci(4))
+
+    print(inefficient_fibonacci(1))
+    print(inefficient_fibonacci(2))
+    print(inefficient_fibonacci(3))
+    print(inefficient_fibonacci(4))
