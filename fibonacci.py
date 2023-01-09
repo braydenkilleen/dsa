86c3941ba344625fb7181b78a88f45842f38b5e3
Brayden Killeen <119263058+braydenkilleen@users.noreply.github.com>
2023-01-09 17:58:47 +1100

    Add iterative fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -14,13 +14,26 @@ def inefficient_fibonacci(n):
     return inefficient_fibonacci(n-2) + inefficient_fibonacci(n-1)
 
 
+def iterative_fibonacci(n):
+    """Compute fibonacci iteratively"""
+    a, b = 0, 1
+    for _ in range(n-1):
+        a, b = b, a+b
+    return b
+
+
 if __name__ == '__main__':
     # print(fibonacci(1))
     # print(fibonacci(2))
     # print(fibonacci(3))
     # print(fibonacci(4))
 
-    print(inefficient_fibonacci(1))
-    print(inefficient_fibonacci(2))
-    print(inefficient_fibonacci(3))
-    print(inefficient_fibonacci(4))
+    # print(inefficient_fibonacci(1))
+    # print(inefficient_fibonacci(2))
+    # print(inefficient_fibonacci(3))
+    # print(inefficient_fibonacci(4))
+
+    print(iterative_fibonacci(1))
+    print(iterative_fibonacci(2))
+    print(iterative_fibonacci(3))
+    print(iterative_fibonacci(4))
