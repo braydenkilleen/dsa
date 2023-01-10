c0335c93aa77b8951628b172dc01bfea00845462
Brayden Killeen <119263058+braydenkilleen@users.noreply.github.com>
2023-01-10 18:38:59 +1100

    Add test

--- knapsack.py
+++ knapsack.py
@@ -1,4 +1,14 @@
-"""Knapsack 0/1 Approaches"""
+"""Knapsack 0/1 Approaches.
+
+An item has value and weight. Knapsack seeks to find a solution which maximises value under capacity/weight constraint.
+
+TODO:
+    - Implement brute force
+    
+"""
+
+
+from typing import Callable
 
 
 class Item:
@@ -8,14 +18,14 @@ class Item:
         self.value = value
         self.weight = weight
 
-    def inverse_weight(self) -> float:
-        return 1.0 / self.weight
-
     def value_density(self) -> float:
         return self.value / self.weight
 
+    def __str__(self) -> str:
+        return f'(name={self.name}, value={self.value}, weight: {self.weight}'
 
-def greedy(items: list[Item], capacity: float, key: function):
+
+def greedy(items: list[Item], capacity: float, key: Callable):
     """Greedy approach to knapsack. 
 
     Running Time: O(n log n) - due to sorted()
@@ -33,12 +43,28 @@ def greedy(items: list[Item], capacity: float, key: function):
         raise ValueError('items must contain an item.')
 
     items_sorted = sorted(items, key=key, reverse=True)
-    result = []
+    selected = []
     total_value, total_weight = 0.0, 0.0
     for i in range(len(items_sorted)):
         if total_weight + items_sorted[i].weight <= capacity:
-            result.append(items_sorted[i])
-            total_weight += items_sorted.weight
+            selected.append(items_sorted[i])
+            total_weight += items_sorted[i].weight
             total_value += items_sorted[i].value
 
-    return (result, total_value)
+    return (selected, total_value)
+
+
+if __name__ == '__main__':
+    items = [
+        Item('a', 100, 10),
+        Item('b', 80, 9),
+        Item('c', 20, 5),
+        Item('d', 75, 4)
+    ]
+    selected, total_value = greedy(items, 35, lambda x: x.value_density())
+
+    print('Selected')
+    for s in selected:
+        print(s)
+
+    print(f'Total value: {total_value}')
