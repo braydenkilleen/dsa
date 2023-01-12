d4893a1ba3a1166eeadb59f702877f0a1d326afb
Brayden Killeen <119263058+braydenkilleen@users.noreply.github.com>
2023-01-12 20:26:26 +1100

    fix node class

--- graph.py
+++ graph.py
@@ -4,7 +4,7 @@
 class Node:
 
     def __init__(self, val):
-        self.val
+        self.val = val
 
     def __str__(self):
         return f'Node({self.val})'
@@ -12,11 +12,11 @@ class Node:
 
 class Edge:
 
-    def __init__(self, source: Node, destination: Node):
+    def __init__(self, source: Node, destination: Node) -> None:
         self.source = source
         self.destination = destination
 
-    def __str__(self):
+    def __str__(self) -> None:
         return f'{self.source} --> {self.destination}'
 
 
@@ -29,3 +29,19 @@ class WeightedEdge(Edge):
 
     def __str__(self):
         return f'{self.source} --{self.weight}--> {self.destination}'
+
+
+class DirectedGraph():
+
+    def __init__(self):
+        self.nodes = []
+        self.edges = {}
+
+    def add_node(self, node):
+        if node in self.ndoes:
+            raise ValueError('Node already exists')
+        self.nodes.append(node)
+        self.edges[node] = []
+
+    def add_edge(self, edge):
+        pass
