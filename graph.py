634775dc9565c278025ef81a557a3a9c14489313
Brayden Killeen <119263058+braydenkilleen@users.noreply.github.com>
2023-01-12 20:22:53 +1100

    Add edge classes

--- graph.py
+++ graph.py
@@ -5,3 +5,24 @@ class Node:
 
     def __str__(self):
         return f'Node({self.val})'
+
+
+class Edge:
+
+    def __init__(self, source: Node, destination: Node):
+        self.source = source
+        self.destination = destination
+
+    def __str__(self):
+        return f'{self.source} --> {self.destination}'
+
+
+class WeightedEdge(Edge):
+
+    def __init__(self, source, destination, weight=1.0):
+        self.source = source
+        self.destination = destination
+        self.weight = weight
+
+    def __str__(self):
+        return f'{self.source} --{self.weight}--> {self.destination}'
